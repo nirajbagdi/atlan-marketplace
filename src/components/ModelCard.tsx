771dc5088d27cbf67364950d847e4d802f67aa08
Niraj Bagdi <bagdiniraj22@gmail.com>
2024-01-30 17:52:49 +0530

    Implemented framer-motion animations for card

--- src/components/ModelCard.tsx
+++ src/components/ModelCard.tsx
@@ -1,10 +1,25 @@
+import { Variants, motion, cubicBezier } from 'framer-motion';
+
 import { ReactComponent as IconDownload } from 'assets/icon-download.svg';
 import { ReactComponent as IconHeart } from 'assets/icon-heart.svg';
 
 import styles from 'styles/components/_ModelCard.module.scss';
 
+const variants: Variants = {
+	hover: {
+		scale: 1.02,
+		boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
+		transition: {
+			duration: 1,
+			ease: cubicBezier(0.165, 0.84, 0.44, 1),
+		},
+	},
+
+	tap: { scale: 0.98 },
+};
+
 const ModelCard: React.FC = () => (
-	<article className={styles.card}>
+	<motion.article variants={variants} whileHover="hover" whileTap="tap" className={styles.card}>
 		<header>
 			<p className={styles.name}>Transformers</p>
 			<span className={styles.category}>Creative</span>
@@ -24,7 +39,7 @@ const ModelCard: React.FC = () => (
 				<IconDownload /> 300+
 			</p>
 		</div>
-	</article>
+	</motion.article>
 );
 
 export default ModelCard;
