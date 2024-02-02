06cd1eae252dd9ff4a7f8d8a2619f27c6d39cf96
Niraj Bagdi <bagdiniraj22@gmail.com>
2024-02-02 20:32:54 +0530

    Improved card animations

--- src/components/Models/ModelsGrid/ModelCard.tsx
+++ src/components/Models/ModelsGrid/ModelCard.tsx
@@ -1,5 +1,4 @@
-import { useRef } from 'react';
-import { Variants, motion, cubicBezier, useScroll, useTransform } from 'framer-motion';
+import { Variants, motion, cubicBezier } from 'framer-motion';
 
 import { TModel } from 'models';
 
@@ -27,44 +26,31 @@ const motionVariants: Variants = {
 	tap: { scale: 0.98 },
 };
 
-const ModelCard: React.FC<Props> = ({ model, shouldTranslate }) => {
-	const articleRef = useRef<HTMLElement>(null);
+const ModelCard: React.FC<Props> = ({ model, shouldTranslate }) => (
+	<motion.article
+		initial={{ translateY: shouldTranslate ? '6.4rem' : '0' }}
+		variants={motionVariants}
+		whileHover="hover"
+		whileTap="tap"
+		className={styles.card}
+	>
+		<header>
+			<p className={styles.name}>{model.name}</p>
+			<span className={styles.category}>{model.category}</span>
+		</header>
 
-	const { scrollYProgress } = useScroll({
-		target: articleRef,
-		offset: ['0 1', '1.33 1'],
-	});
+		<p className={styles.summary}>{model.summary}</p>
 
-	const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.75, 1]);
+		<div className={styles.stats}>
+			<p className={styles.heart}>
+				<HeartIcon /> {model.stats.likes}
+			</p>
 
-	return (
-		<motion.article
-			initial={{ translateY: shouldTranslate ? '6.4rem' : '0' }}
-			style={{ scale: scaleProgress }}
-			variants={motionVariants}
-			whileHover="hover"
-			whileTap="tap"
-			className={styles.card}
-			ref={articleRef}
-		>
-			<header>
-				<p className={styles.name}>{model.name}</p>
-				<span className={styles.category}>{model.category}</span>
-			</header>
-
-			<p className={styles.summary}>{model.summary}</p>
-
-			<div className={styles.stats}>
-				<p className={styles.heart}>
-					<HeartIcon /> {model.stats.likes}
-				</p>
-
-				<p className={styles.download}>
-					<DownloadIcon /> {model.stats.downloads}
-				</p>
-			</div>
-		</motion.article>
-	);
-};
+			<p className={styles.download}>
+				<DownloadIcon /> {model.stats.downloads}
+			</p>
+		</div>
+	</motion.article>
+);
 
 export default ModelCard;
