cebe5a0c6b23119df0a3e40ba59e913fe05ad070
Niraj Bagdi <bagdiniraj22@gmail.com>
2024-02-03 19:28:55 +0530

    scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Home from 'pages/Home';
 import SingleModel from 'pages/SingleModel';
 
-const App: React.FC = () => (
-	<Routes>
-		<Route index element={<Home />} />
-		<Route path="/models/:modelSlug" element={<SingleModel />} />
-	</Routes>
-);
+const App: React.FC = () => {
+	const location = useLocation();
+
+	// Scroll to top
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [location.pathname]);
+
+	return (
+		<Routes location={location} key={location.pathname}>
+			<Route index element={<Home />} />
+			<Route path="/models/:modelSlug" element={<SingleModel />} />
+		</Routes>
+	);
+};
 
 export default App;
