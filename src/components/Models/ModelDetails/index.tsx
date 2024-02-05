80bb89cc934736ab926a230a2b2515b26702aca6
Niraj Bagdi <bagdiniraj22@gmail.com>
2024-02-05 14:52:51 +0530

    Added the TryOut button in ModelDetails

--- src/components/Models/ModelDetails/index.tsx
+++ src/components/Models/ModelDetails/index.tsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 import { TModel } from 'models';
 import ModelDetailsAside from './ModelDetailsAside';
+import { ReactComponent as ExperimentIcon } from 'assets/experiment-icon.svg';
 
 import styles from 'styles/components/_ModelDetails.module.scss';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 type Props = {
 	model: TModel;
@@ -17,6 +19,12 @@ const ModelDetails: React.FC<Props> = ({ model }) => (
 			<h1>{model.name}</h1>
 			<span>{model.provider}</span>
 			<p>{model.summary}</p>
+
+			<button>
+				<Link to={`/models/${model.slug}/try`}>
+					<ExperimentIcon /> Try It Out
+				</Link>
+			</button>
 		</header>
 
 		<div className={styles.main}>
