019b238487114920dcd573b0830d0580a1046c17
nirajbagdi <bagdiniraj22@gmail.com>
2024-08-08 23:21:54 +0530

    added mock json data locally

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,17 @@
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError } from "axios";
 
-import { TModel } from 'models';
+import { TModel } from "models";
 
-const URL = 'https://mocki.io/v1/2f6128ec-0d2c-468b-be35-5e1f48df1e3f';
+import MODELS from "data/models.json";
+
+const URL = "https://mocki.io/v1/2f6128ec-0d2c-468b-be35-5e1f48df1e3f";
 
 export async function fetchModels(): Promise<TModel[]> {
-	try {
-		const response = await axios.get(URL);
-		return response.data;
-	} catch (error) {
-		throw (error as AxiosError).message;
-	}
+	return MODELS;
+	// try {
+	// 	const response = await axios.get(URL);
+	// 	return response.data;
+	// } catch (error) {
+	// 	throw (error as AxiosError).message;
+	// }
 }
