6a062760a5d8f2ead749fe0aa00f11bcba511369
nirajbagdi <bagdiniraj22@gmail.com>
2024-08-08 23:33:50 +0530

    updated mock.io path

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,16 +2,13 @@ import axios, { AxiosError } from "axios";
 
 import { TModel } from "models";
 
-import MODELS from "data/models.json";
-
-const URL = "https://mocki.io/v1/2f6128ec-0d2c-468b-be35-5e1f48df1e3f";
+const URL = "https://mocki.io/v1/2f678e1b-aab9-4fc7-b2f1-d62b3f426be5";
 
 export async function fetchModels(): Promise<TModel[]> {
-	return MODELS;
-	// try {
-	// 	const response = await axios.get(URL);
-	// 	return response.data;
-	// } catch (error) {
-	// 	throw (error as AxiosError).message;
-	// }
+	try {
+		const response = await axios.get(URL);
+		return response.data;
+	} catch (error) {
+		throw (error as AxiosError).message;
+	}
 }
